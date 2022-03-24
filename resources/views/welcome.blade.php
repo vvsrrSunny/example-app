9c7d13d5a93af15666398061a24812145e6dd4ed
Sunny Varanasi <Sunny.Varanasi@pentanet.com.au>
2022-03-24 11:55:56 +0800

    modified new config changes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,8 +4,8 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <script src="{{ asset('js/app.js') }}"></script>
+    <link href="{{ asset('asserts/css/app.css') }}" rel="stylesheet">
+    <script src="{{ asset('asserts/js/app.js') }}"></script>
     <title>Laravel</title>
 
     <!-- Fonts -->
