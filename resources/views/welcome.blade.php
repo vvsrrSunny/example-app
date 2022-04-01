f77a047081079f44233d21ac8d5ded1582d45e55
Sunny Varanasi <Sunny.Varanasi@pentanet.com.au>
2022-04-01 12:19:21 +0800

    modularizing the balde files

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,5 @@
 <!DOCTYPE html>
+
 <x-layouts.app>
     <div class="container">
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark m-5">
@@ -37,45 +38,5 @@
         </nav>
     </div>
 
-    <div>
-        <div class="container-fluid custombg mt-5">
-            <div class="container py-5">
-                <h1 class="text-light fw-bold"> This is about me</h1>
-                <p class="text-light subhead fs-7  mb-0">This is my website and gonna show my details</p>
-            </div>
-        </div>
-
-        <div class="container my-cards">
-            <div class="row mt-5 gap-4">
-                <div class="card col-sm bg-gray-100">
-                    <img src="{{URL::to('/')}}\images\personal-project.jpg" class="card-img-top" alt="...">
-
-                    <div class="card-body">
-                        <h5 class="card-title text-light">My personal projects</h5>
-                        <p class="card-text text-gray-500">My personal projects include my website, algorithms and good softwares.</p>
-                        <a href="#" class="btn btn-primary">Want to know more</a>
-                    </div>
-                </div>
-
-                <div class="card col-sm bg-gray-900">
-                    <img src="{{URL::to('/')}}\images\career.jpeg" class="card-img-top" alt="...">
-
-                    <div class="card-body">
-                        <h5 class="card-title text-light">My work in companies</h5>
-                        <p class="card-text text-gray-500">This shows my experience in all the companies and my work there.</p>
-                        <a href="#" class="btn btn-primary">Want to know more</a>
-                    </div>
-                </div>
-
-                <div class="card col-sm bg-gray-900">
-                    <img src="{{URL::to('/')}}\images\hobbies.jpg" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title text-light">My hobbies</h5>
-                        <p class="card-text text-gray-500">My hobbies include table tennis, cricket, chess and kabaddi.</p>
-                        <a href="#" class="btn btn-primary">Want to know more</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+<x-home/>
 </x-layouts.app>
