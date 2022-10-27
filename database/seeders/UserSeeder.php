04981cdf1ab369a3a310e2a76ed9b092ee041124
vvsrrSunny <bharadwaj.varanasi96@gmail.com>
2022-10-27 12:11:18 +0800

    added a seeded file so we can now run seeeder command and add it to docker

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,8 +2,9 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class UserSeeder extends Seeder
 {
@@ -14,6 +15,8 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        //
+        User::factory()
+            ->count(4)
+            ->create();
     }
 }
